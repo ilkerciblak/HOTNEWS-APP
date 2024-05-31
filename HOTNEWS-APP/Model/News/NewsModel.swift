af10cfec017c4bf9bdd113823497688b93f63431
ilkerciblak <ilkerciblak@gmail.com>
2024-06-01 00:59:38 +0300

    chore(model): introduce news model

---
+++ HOTNEWS-APP/Model/News/NewsModel.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+
+// MARK: - NewsResponse
+struct NewsResponse: Decodable {
+    let status: String?
+    let totalResults: Int?
+    let articles: [Article]?
+}
+
+// MARK: - Article
+struct Article: Decodable {
+    let source: Source
+    let author, title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
+}
+
+// MARK: - Source
+struct Source: Decodable {
+    let id: String?
+    let name: String?
+}
+
+
