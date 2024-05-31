a112be6b4488543d78f6cbe2ae4e260ebb32e0b6
ilkerciblak <ilkerciblak@gmail.com>
2024-06-01 00:28:18 +0300

    Initial Commit

---
+++ HOTNEWS-APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HOTNEWS-APP
+//
+//  Created by İlker Cıblak on 1.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
